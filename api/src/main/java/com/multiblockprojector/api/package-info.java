8cbc7554c796af3cef6909a40e67759695b5c0c8
zhintze <zachhintze@gmail.com>
2026-02-17 22:53:46 -0700

    feat: scaffold API Gradle subproject

---
+++ api/src/main/java/com/multiblockprojector/api/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Public API for the Multiblock Projector mod.
+ * Third-party mods depend on this module to register their multiblocks.
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.multiblockprojector.api;
